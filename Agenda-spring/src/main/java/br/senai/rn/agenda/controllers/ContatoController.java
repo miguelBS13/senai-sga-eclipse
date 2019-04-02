8e9bfa45a7a2fbfac16c72d897d910a01e98c062
Miguel Barbosa <miguelbarbosa1309@gmail.com>
2019-04-02 17:00:19 -0300

    mudei 02/04

--- Agenda-spring/src/main/java/br/senai/rn/agenda/controllers/ContatoController.java
+++ Agenda-spring/src/main/java/br/senai/rn/agenda/controllers/ContatoController.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.senai.rn.agenda.model.Contato;
 import br.senai.rn.agenda.services.ContatoService;
 
 @Controller
-@RequestMapping("/fone")
+@RequestMapping("/")
 public class ContatoController {
 	
 	@Autowired
@@ -17,7 +20,34 @@ public class ContatoController {
 	
 	@GetMapping
 	public String index(Model model) {
+		model.addAttribute("contato",new Contato());
 		model.addAttribute("contatos", service.BuscarTodos());
 		return "index";
 	}
+	
+	@PostMapping
+	public String salvar(Contato contato) {
+		service.Salvar(contato);
+		return "redirect:/";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String editar(@PathVariable Long id, Model model) {
+		model.addAttribute("contato",service.Buscar(id));
+		model.addAttribute("contatos", service.BuscarTodos());
+		return "index";
+	}
+	
+	@PostMapping("/editar/{id}")
+	public String save(@PathVariable Long id, Contato contato) {
+		service.Salvar(contato);
+		return "redirect:/";
+	}
+	
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable Long id, Contato contato) {
+		service.remover(contato);
+		return "redirect:/";
+	}
+	
 }
