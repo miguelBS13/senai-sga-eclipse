95de40dc52ef449bbd5db1a8c1e1cadab2e1be89
Miguel Barbosa <miguelbarbosa1309@gmail.com>
2019-04-10 16:40:08 -0300

    mudei 10/04

--- Locadora-spring/src/main/java/br/senai/rn/locadora/controllers/FilmeController.java
+++ Locadora-spring/src/main/java/br/senai/rn/locadora/controllers/FilmeController.java
@@ -7,44 +7,31 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.senai.rn.locadora.model.Filme;
-import br.senai.rn.locadora.services.FilmeService;
+import br.senai.rn.locadora.model.Locadora;
+import br.senai.rn.locadora.services.LocadoraService;
 
 @Controller
 @RequestMapping("/filme")
-public class FilmeController {
-
+public class FilmeController extends AbstractController<Filme> {
+	
 	@Autowired
-	private FilmeService service;
+	private LocadoraService categoriaService;
 	
-	@GetMapping
+	@Override
 	public String index(Model model) {
-		Filme filme = new Filme();
-		List<Filme> filmes = service.obterTodos();
-		model.addAttribute("filme", filme);
-		model.addAttribute("filmes", filmes);
-		return "filme/index2";
+		List<Locadora> locadoras = categoriaService.obterTodos();
+		model.addAttribute("locadoraLista", locadoras);
+		return super.index(model);
 	}
 	
-	@PostMapping
-	public String salvar(Filme filme) {
-		service.salvar(filme);
-		return "redirect:/filme";
-	}
-	
-	@GetMapping("/remover/{id}")
-	public String remover(@PathVariable Long id, Model model) {
-		service.remover(id);
-		return "redirect:/filme";
-	}
-	
+	@Override
 	@GetMapping("/editar/{id}")
 	public String editar(@PathVariable Long id, Model model) {
-		model.addAttribute("filme",service.obter(id));
-		model.addAttribute("filmes", service.obterTodos());		
-		return "filme/index2";
+		List<Locadora> locadoras = categoriaService.obterTodos();
+		model.addAttribute("locadoraLista", locadoras);
+		return super.editar(id, model);
 	}
 }
--- Locadora-spring/src/main/java/br/senai/rn/locadora/controllers/LocadoraController.java
+++ Locadora-spring/src/main/java/br/senai/rn/locadora/controllers/LocadoraController.java
@@ -1,39 +1,12 @@
 package br.senai.rn.locadora.controllers;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.senai.rn.locadora.model.Locadora;
-import br.senai.rn.locadora.services.LocadoraService;
 
 @Controller
 @RequestMapping("/locadora")
-public class LocadoraController {
-
-	@Autowired
-	private LocadoraService service;
+public class LocadoraController extends AbstractController<Locadora> {}
 	
-	@GetMapping
-	public String index(Model model) {
-		Locadora locadora = new Locadora();
-		List<Locadora> locadoras = service.obterTodos();
-		model.addAttribute("locadora", locadora);
-		model.addAttribute("locadoras", locadoras);
-		return "categoria/index";
-	}
-	
-	@PostMapping
-	public String salvar(Locadora locadora) {
-		service.salvar(locadora);
-		return "redirect:/locadora";
-	}
-	
-	
-	
-}
